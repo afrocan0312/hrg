896df11da5b3dd0a159d0512f554d5f0cc157684
afrocan0312 <afrocan0312@ajou.ac.kr>
2017-11-17 15:12:07 +0900

    1117_2차개발
    ending_씬 전환
    터치 구현

---
+++ HRiding_2/Assets/scenechange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechange : MonoBehaviour {
+
+
+	public void SceneChange1(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("add_object");
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+}
+}
